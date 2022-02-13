17462e347ed449d066333afd3fbfb418e62ab3ff
bulyts01 <bulytovs@mail.ru>
2022-02-13 13:11:07 +0800

    5. Аннотация @Override [#174891]

--- src/main/java/ru/job4j/inheritance/JSONReport.java
+++ src/main/java/ru/job4j/inheritance/JSONReport.java
@@ -1,6 +1,8 @@
 package ru.job4j.inheritance;
 
 public class JSONReport extends TextReport {
+
+    @Override
     public String generate(String name, String body) {
         String tab = "\t";
         String ln = System.lineSeparator();
