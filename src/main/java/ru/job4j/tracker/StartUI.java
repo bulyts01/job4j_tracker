6c02bf6f763361ba80fcdd692512a894855d8d47
bulyts01 <bulytovs@mail.ru>
2022-02-18 13:09:23 +0800

    6. Date. Отображение даты. [#310017 #264529]

--- src/main/java/ru/job4j/tracker/StartUI.java
+++ src/main/java/ru/job4j/tracker/StartUI.java
@@ -4,6 +4,7 @@ import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 
 public class StartUI {
+
     public static void main(String[] args) {
         DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd-MMMM-EEEE-yyyy HH:mm:ss");
         Item item = new Item();
