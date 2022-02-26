02b455a32e512cf7514c4d5022a0e6d15cf91d23
bulyts01 <bulytovs@mail.ru>
2022-02-26 17:46:22 +0800

    5.1. Тестирование. Подготовка данных. [#182960]

--- src/test/java/ru/job4j/tracker/StartUITest.java
+++ src/test/java/ru/job4j/tracker/StartUITest.java
@@ -39,8 +39,8 @@ public class StartUITest {
                 String.valueOf(item.getId()),
                 "edited item"
         };
-        StartUI.editItem(new StubInput(answers), tracker);
-        Item edited = tracker.findById(item.getId());
-        assertThat(edited.getName(), is("edited item"));
+        StartUI.deleteItem(new StubInput(answers), tracker);
+        Item deleted = tracker.findById(item.getId());
+        assertThat(deleted.getName(), is("null"));
     }
 }
