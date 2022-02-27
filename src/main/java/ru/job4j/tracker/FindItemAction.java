e6945b9a0c68a378b050b5de44a8f97b57e34f2b
bulyts01 <bulytovs@mail.ru>
2022-02-27 11:09:21 +0800

    8. Реализация меню за счет шаблона стратегия. [#181780]

---
+++ src/main/java/ru/job4j/tracker/FindItemAction.java
@@ -0,0 +1,21 @@
+package ru.job4j.tracker;
+
+public class FindItemAction implements UserAction {
+    @Override
+    public String name() {
+        return "Find item by ID";
+    }
+
+    @Override
+    public boolean execute(Input input, Tracker tracker) {
+        System.out.println("=== Find ID ===");
+        int id = input.askInt("Enter ID: ");
+        Item item = tracker.findById(id);
+        if (item != null) {
+            System.out.println(item);
+        } else {
+            System.out.println("Заявка с введенным id: " + id + " не найдена");
+        }
+        return true;
+    }
+}
