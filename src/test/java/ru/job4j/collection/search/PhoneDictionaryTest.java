7da18e3bd7a918b8228a548d20e6dfa2ec44e058
Bulytov Semen <bulytovs@mail.ru>
2025-04-24 05:16:07 +0800

    4. Телефонный справочник на базе ArrayList [#41598]

---
+++ src/test/java/ru/job4j/collection/search/PhoneDictionaryTest.java
@@ -0,0 +1,30 @@
+package ru.job4j.collection.search;
+
+import org.junit.Test;
+
+import java.util.ArrayList;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.assertThat;
+
+public class PhoneDictionaryTest {
+    @Test
+    public void whenFindByName() {
+        PhoneDictionary phones = new PhoneDictionary();
+        phones.add(
+                new Person("Petr", "Arsentev", "534872", "Bryansk")
+        );
+        ArrayList<Person> persons = phones.find("Petr");
+        assertThat(persons.get(0).getSurname(), is("Arsentev"));
+    }
+
+    @Test
+    public void whenNotFound() {
+        PhoneDictionary phones = new PhoneDictionary();
+        phones.add(
+                new Person("Petr", "Arsentev", "534872", "Bryansk")
+        );
+        ArrayList<Person> persons = phones.find("Semen");
+        assertThat(persons.size(), is(0));
+    }
+}
