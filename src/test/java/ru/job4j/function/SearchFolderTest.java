74cb9e3266ffd7e7a381567075465e86b06d173a
Semen <semen.bulytov@mail.ru>
2025-05-16 17:20:51 +0800

    2.3. Встроенные функциональные интерфейсы. [#249209]

---
+++ src/test/java/ru/job4j/function/SearchFolderTest.java
@@ -0,0 +1,42 @@
+package ru.job4j.function;
+
+import org.junit.Test;
+
+import java.util.List;
+import java.util.function.Predicate;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class SearchFolderTest {
+
+    @Test
+    public void whenFilterSize() {
+        List<Folder> folders = List.of(
+                new Folder("fix", 110),
+                new Folder("bug", 75),
+                new Folder("bug", 90)
+        );
+
+        List<Folder> expected = List.of(new Folder("fix", 110));
+        Predicate<Folder> predicate = folder -> folder.getSize() > 100;
+        List<Folder> result = SearchFolder.filter(folders, predicate);
+
+        assertThat(result).containsExactlyElementsOf(expected);
+    }
+
+    @Test
+    public void whenFilterName() {
+        List<Folder> list = List.of(
+                new Folder("fix", 110),
+                new Folder("bug", 75),
+                new Folder("bug", 90)
+        );
+        List<Folder> expected = List.of(
+                new Folder("bug", 75),
+                new Folder("bug", 90)
+        );
+        Predicate<Folder> predicate = folder -> folder.getName().contains("bug");
+        List<Folder> result = SearchFolder.filter(list, predicate);
+        assertThat(result).containsAll(expected);
+    }
+}
