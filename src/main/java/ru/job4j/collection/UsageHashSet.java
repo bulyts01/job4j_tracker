f1d83d04b399dc348a7bee33fbb639f75d0a20d2
Semen <semen.bulytov@mail.ru>
2025-05-26 03:29:29 +0800

    0. Set, HashSet, Iterator. [#10095]

---
+++ src/main/java/ru/job4j/collection/UsageHashSet.java
@@ -0,0 +1,18 @@
+package ru.job4j.collection;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class UsageHashSet {
+    public static void main(String[] args) {
+        Set<String> cars = new HashSet<>();
+        cars.add("Toyota");
+        cars.add("Lexus");
+        cars.add("BMW");
+        cars.add("Audi");
+        cars.add("Mercedes");
+        for (String car : cars) {
+            System.out.println(car);
+        }
+    }
+}
