6e26c1b1966f5dc8e930b0384d4cc890f35e28c6
Semen <semen.bulytov@mail.ru>
2025-06-07 14:59:05 +0800

    0. Map, HashMap [#212652]

---
+++ src/main/java/ru/job4j/collection/UsageMap.java
@@ -0,0 +1,19 @@
+package ru.job4j.collection;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class UsageMap {
+    public static void main(String[] args) {
+        Map<String, String> hashMap = new HashMap<>();
+        hashMap.put("semen@mail.ru", "Semen Bulytov");
+        hashMap.put("petr@mail.ru", "Petr Arsentiev");
+        hashMap.put("stas@mail.ru", "Stas Korobeinikov");
+        hashMap.put("aleksei@mail.ru", "Aleksie Alekseev");
+        hashMap.put("ivan@mail.ru", "Ivan Ivanov");
+
+        for (String key : hashMap.keySet()) {
+            System.out.println(key + " = " + hashMap.get(key));
+        }
+    }
+}
